db902e65ca5f33187f1001d595901dcef9300225
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-05-09 13:14:30 +0300

    Create update-cosmos.sh

---
+++ update-cosmos.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+
+# Logo
+sleep 1 && curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/main/logo.sh | bash && sleep 1
+
+# Update
+echo -e "\e[1m\e[32m1. Update... \e[0m" && sleep 1
+sudo apt update && sudo apt upgrade -y
+sleep 1
+
+# Package
+echo -e "\e[1m\e[32m2. Installing package... \e[0m" && sleep 1
+sudo apt install curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential protobuf-compiler bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+sleep 1
+
+# Check if Go is installed and remove it
+echo -e "\e[1m\e[32m3. Removing old Go versions... \e[0m" && sleep 1
+sudo rm -rf /usr/local/go
+sudo rm /etc/paths.d/go
+sudo apt-get remove golang-go
+sudo apt-get remove --auto-remove golang-go
+
+# Go
+echo -e "\e[1m\e[32m4. Installing Go... \e[0m" && sleep 1
+VER="1.21.3"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+cd $HOME
+
+echo -e "\e[1m\e[32mFINISH \e[0m" && sleep 1
