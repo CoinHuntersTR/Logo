113ee2feece72f20e3b2009545b9a2c22e5d6b05
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-05-09 13:12:20 +0300

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,6 @@
+echo -e "\033[0;35m"
+echo "//==========================================================================\\"
+echo "||COIN HUNTERS NODE                                                           ||"
+echo "||👑 Crypto Community & Validator Service & Startup Acc.Web3 DeFi Blockchain Tech Investor, Researcher and Content Creator ||"
+echo "\\==========================================================================//"
+echo -e "\e[0m"
