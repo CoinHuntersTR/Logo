a2ff0c88c563ecf0dcaf899881e7c6bc5999b84e
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-08-04 17:19:18 +0300

    Update dependencies_install.sh

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-source <(curl -s https://raw.githubusercontent.com/CoinHuntersTR/Logo/main/common.sh)
+source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/common.sh)
 
 printGreen "2. Updating packages..." && sleep 1
 sudo apt update
+sudo apt-get update
 
 printGreen "3. installing dependencies..." && sleep 1
 sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+sudo apt-get install -y libssl-dev
