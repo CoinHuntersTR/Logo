f82c5377aaf1b67da03131b881ed73e6020c5a3a
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-10-17 10:05:11 +0300

    Update dependencies_install.sh

--- dependencies_install.sh
+++ dependencies_install.sh
@@ -5,6 +5,5 @@ printGreen "2. Updating packages..." && sleep 1
 sudo apt update
 sudo apt-get update
 
-printGreen "3. installing dependencies..." && sleep 1
-sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
-sudo apt-get install -y libssl-dev
+printGreen "3. Installing dependencies..." && sleep 1
+sudo apt install -y curl git wget htop tmux build-essential jq make lz4 gcc unzip libssl-dev tar clang pkg-config bsdmainutils ncdu chrony liblz4-tool
